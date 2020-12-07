5648439e611c11f9f998034612c98545ea1165d0
Lois Simonca <loisgabor@gmail.com>
2020-12-07 13:30:11 -0500

    added some methods

--- connection.js
+++ connection.js
@@ -1,6 +1,219 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+const employeeArray = [];
+const managerArray = [];
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "LoisElizabeth123",
+  database: "employee_db",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  start();
+  //   afterConnection();
+});
+const getManager = () => {
+  connection.query(
+    "SELECT * FROM employee WHERE manager_id is null",
+    (err, res) => {
+      if (err) throw err;
+      // console.log("THIS IS THE RESPONSE");
+      // console.log(res);
+      const managerArray = res.map((manager) => {
+        return {
+          name: `${manager.first_name} ${manager.last_name}`,
+          value: manager.id,
+        };
+      });
+      console.log(managerArray);
+      return managerArray;
+    }
+  );
+};
+
+// function afterConnection() {
+//   connection.query("SELECT * FROM products", function (err, res) {
+//     if (err) throw err;
+//     console.log(res);
+//     connection.end();
+//   });
+// }
+
+function start() {
+  inquirer.prompt(introQuestion).then((data) => {
+    switch (data.firstQuestion) {
+      case "View all employees":
+        viewEmployees();
+        break;
+      case "View all employees by department":
+        break;
+      case "View all employees by manager":
+        break;
+      case "Add employee":
+        addEmployee();
+        break;
+      case "Update employee role":
+        updateEmployee();
+        break;
+      case "Add department":
+        addDepartment();
+        break;
+      case "Add role":
+        addRoles();
+        break;
+    }
+  });
+}
+
+const viewEmployees = () => {
+  connection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    console.table("______________________________", res);
+    start();
+  });
+};
+
+const addEmployee = () => {
+  connection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    else {
+      res.forEach((row) => {
+        console.log(row.first_name);
+        managerArray.push({
+          name: row.first_name + " " + row.last_name,
+          value: row.emp_id,
+        });
+      });
+      console.log(managerArray);
+      inquirer.prompt(addEmployeeQuestions).then(function (data) {
+        connection.query("INSERT INTO employee SET ?", {
+          first_name: data.employeeFirstName,
+          last_name: data.employeeLastName,
+          role_id: data.role_id,
+          manager_id: data.employeeManager,
+        });
+        employeeArray.push(data);
+        console.table(employeeArray);
+        start();
+      });
+    }
+  });
+  // console.log(managerArray);
+  // inquirer.prompt(addEmployeeQuestions).then(function (data) {
+  //   connection.query("INSERT INTO employee SET ?", {
+  //     first_name: data.employeeFirstName,
+  //     last_name: data.employeeLastName,
+  //     role_id: role_id,
+  //     manager_id: data.employeeManager,
+  //   });
+  //   employeeArray.push(data);
+  //   console.table(employeeArray);
+  //   start();
+  // });
+};
+const updateEmployee = () => {
+  connection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    else {
+      res.forEach((row) => {
+        console.log(row.first_name);
+        employeeArray.push({
+          name: row.first_name + " " + row.last_name,
+          value: row.id,
+        });
+      });
+      // console.log(managerArray);
+      inquirer.prompt(updateEmployeeQuestions).then(function (data) {
+        connection.query("UPDATE employee SET role_id = ? WHERE id = ?;", [
+          data.employeeUpdateRole,
+          data.employeeUpdate,
+        ]);
+        // employeeArray.push(data);
+        // console.table(employeeArray);
+        start();
+      });
+    }
+  });
+};
+const addDepartment = () => {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "What department would you like to add?",
+      name: "departmentName",
+    })
+    .then(function (data) {
+      connection.query("INSERT INTO department SET ?", {
+        name: data.departmentName,
+      });
+      start();
+    });
+};
+
+const addRoles = () => {
+  let departmentArray = [];
+  connection.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    else {
+      res.forEach((row) => {
+        // console.log(row.first_name);
+        departmentArray.push({
+          name: row.name,
+          value: row.id,
+        });
+      });
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What role would you like to add?",
+            name: "newTitle",
+          },
+          {
+            type: "input",
+            message: "What is the salary for this role?",
+            name: "salary",
+          },
+          {
+            type: "list",
+            message: "What is the department id?",
+            name: "departmentId",
+            choices: departmentArray,
+          },
+        ])
+        .then(function (data) {
+          connection.query("INSERT INTO role SET ?", {
+            title: data.newTitle,
+            salary: data.salary,
+            department_id: data.departmentId,
+          });
+          start();
+        });
+    }
+  });
+};
+// const removeEmployee = () => {
+//     inquirer.prompt({
+//     type: "list",
+//     name: "employeeRemoval",
+//     message: "Which employee would you like to remove?",
+//     choices: employeeArray
+//   }).then(function (data) {
+//       connection.query("DELETE FROM employee WHERE id=?", (err, res) => {
+//         if (err) throw err;
+//         console.table(employeeArray);
+//           .then(delete employeeArray[res]);
+//         // .then(start());
+//       });
+//     });
+// };
+
 const introQuestion = {
   type: "list",
   name: "firstQuestion",
@@ -10,13 +223,13 @@ const introQuestion = {
     "View all employees by department",
     "View all employees by manager",
     "Add employee",
-    "Remove employee",
+    "Add department",
+    "Add role",
     "Update employee role",
-    "Update employee manager",
     "Exit",
   ],
 };
-const addEmployee = [
+const addEmployeeQuestions = [
   {
     type: "input",
     name: "employeeFirstName",
@@ -41,116 +254,33 @@ const addEmployee = [
       "Legal Team Lead",
     ],
   },
-];
-
-const updateEmployee = [
   {
-    type: "input",
-    name: "employeeUpdateManager",
-    message: "Which employee's manager do you want to update",
-  },
-  {
-    type: "input",
-    name: "employeeUpdateManager",
-    message:
-      "Which employee do you want to set as manager for the selected employee",
+    type: "list",
+    message: "Who is the employee's manager",
+    choices: managerArray,
+    name: "employeeManager",
   },
 ];
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "LoisElizabeth123",
-  database: "employee_db",
-});
-
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  start();
-  //   afterConnection();
-});
-
-// function afterConnection() {
-//   connection.query("SELECT * FROM products", function (err, res) {
-//     if (err) throw err;
-//     console.log(res);
-//     connection.end();
-//   });
-// }
-
-function start() {
-  inquirer.prompt(introQuestion).then((data) => {
-    switch (data.firstQuestion) {
-      case "View all employees":
-        connection.query("SELECT * FROM employee", (err, res) => {
-          if (err) throw err;
-          console.table(res);
-          start();
-        });
-        break;
-      case "View all employees by department":
-        break;
-      case "View all employees by manager":
-        break;
-      case "Add employee":
-        console.log("this worked");
-
-        inquirer
-          .prompt(addEmployee)
-          // .then(inquirer.prompt(introQuestion));
-
-          .then(function (data) {
-            connection.query("INSERT INTO employee SET ?", {
-              first_name: data.employeeFirstName,
-              last_name: data.employeeLastName,
-            });
-            console.table(data);
-            start();
-          });
-        // });
-        break;
-      case "Remove employee":
-        // var removal = (data) =>{
-        // connection.query("SELECT * FROM employee", (err, res) => {
-        // inquirer.prompt({
-        //   type: "list",
-        //   name: "employeeRemoval",
-        //   message: "Which employee would you like to remove?",
-        //   choices: function (res) {
-        //     var employeeArray = [];
-        //     for (var i = 0; i < data.length; i++) {
-        //       employeeArray.push(data[i].first_name, last_name);
-        //     }
-        //     return employeeArray;
-        //   },
-        // });
-
-        connection.query("DELETE FROM employee", (err, res) => {
-          if (err) throw err;
-
-          inquirer.prompt({
-            type: "list",
-            name: "employeeRemoval",
-            message: "Which employee would you like to remove?",
-            choices: function (value) {
-              var employeeArray = [];
-              for (var i = 0; i < res.length; i++) {
-                employeeArray.push(res[i].first_name, last_name);
-              }
-              return employeeArray;
-            },
-          });
-        });
-
-        // start();
-
-        break;
-      case "Update employee role":
-        break;
-      case "Update employee manager":
-        break;
-    }
-  });
-}
+const updateEmployeeQuestions = [
+  {
+    type: "list",
+    name: "employeeUpdate",
+    message: "Which employee's role would you like to update",
+    choices: employeeArray,
+  },
+  {
+    type: "list",
+    name: "employeeUpdateRole",
+    message: "What is the employee's new role?",
+    choices: [
+      "Sales Lead",
+      "Salesperson",
+      "Lead Engineer",
+      "Software Engineer",
+      "Account Manager",
+      "Accountant",
+      "Legal Team Lead",
+    ],
+  },
+];
